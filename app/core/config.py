ae0a5095bbc07e4c036b6dd85378c66c364dc087
Albert Hahn <albert.post@mailbox.org>
2024-03-18 22:59:56 +0100

    changed name

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ import secrets
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Literal
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_ignore_empty=True, extra="ignore"
@@ -10,7 +11,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
-    PROJECT_NAME: str = "case_study_ssp"
-    
+    PROJECT_NAME: str = "case_study_backend"
+
 
 settings = Settings()
